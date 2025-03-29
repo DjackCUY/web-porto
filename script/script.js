7e0ff75914a709e14172768b7f68817f229fab46
Adz Dzaka <cuylearn@gmail.com>
2025-03-29 13:15:14 +0700

    Memperbarui project dengan menghilangkan JavaScript

--- script/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navbar = document.querySelector(".ul-navbar");
-
-    hamburger.addEventListener("click", function () {
-        navbar.classList.toggle("active");
-    });
-});
